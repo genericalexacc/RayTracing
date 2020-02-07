e7505b8d0d3051813920f9bdfefa02d110138cad
Alexander Shevchenko <alex.shevchenko@ubico.io>
2020-02-06 21:11:28 -0500

    Initial Commit

---
+++ Raytracing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Raytracing
+//
+//  Created by Alexander Shevchenko on 2020-02-06.
+//  Copyright © 2020 Alexander Shevchenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
