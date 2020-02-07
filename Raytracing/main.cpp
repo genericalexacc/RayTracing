7111501cf432eebb2da2118cbd2da2ba22eb7e7f
Alexander Shevchenko <alex.shevchenko@ubico.io>
2020-02-07 15:03:56 -0500

    Working dielectrics

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -232,14 +232,74 @@ vec3 reflect(const vec3& v, const vec3& n){
 
 class metal : public material {
 public:
-    metal(const vec3& a) : albedo(a) {}
+    metal(const vec3& a, float f) : albedo(a) {if (f < 1) { fuzz = f; }else{ fuzz=1; }}
     virtual bool scatter(const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered) const {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected);
+        scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
         attenuation = albedo;
         return (dot(scattered.direction(), rec.normal) > 0);
     }
     vec3 albedo;
+    float fuzz;
+};
+
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted){
+    vec3 uv = unit_vector(v);
+    float dt = dot(uv, n);
+    float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1-dt*dt);
+    if(discriminant > 0){
+        refracted = ni_over_nt*(uv-n*dt) - n * sqrt(discriminant);
+        return true;
+    }else{
+        return false;
+    }
+}
+
+float schlick(float cosine, float ref_idx){
+    float r0 = (1-ref_idx) / (1+ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1-r0) * pow((1-cosine), 5);
+}
+
+class dielectric : public material {
+public:
+    dielectric(float ri) : ref_idx(ri){};
+    virtual bool scatter(const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered) const {
+        vec3 outward_normal;
+        vec3 reflected = reflect(r_in.direction(), outward_normal);
+        float ni_over_nt;
+        attenuation = vec3(1.0, 1.0, 1.0);
+        vec3 refracted;
+        
+        float reflected_prob;
+        float cosine;
+        
+        if(dot(r_in.direction(), rec.normal) > 0){
+            outward_normal = -rec.normal;
+            ni_over_nt = ref_idx;
+            cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+        }else{
+            outward_normal = rec.normal;
+            ni_over_nt = 1.0 / ref_idx;
+            cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+        }
+        if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted)){
+            scattered = ray(rec.p, refracted);
+            reflected_prob = schlick(cosine, ref_idx);
+        }else{
+            reflected_prob = 1.0;
+        }
+        if(random_double() < reflected_prob){
+            scattered = ray(rec.p, reflected);
+        }else{
+            scattered = ray(rec.p, refracted);
+        }
+        
+        return true;
+    }
+    
+    
+    float ref_idx;
 };
 
 class sphere : public hitable{
@@ -352,13 +412,14 @@ int main(int argc, const char * argv[]) {
     myfile.open ("/Users/alexandershevchenko/Desktop/out.ppm");
     myfile << "P3\n" << nx << " " << ny << "\n255\n";
     
-    hitable *list[4];
+    hitable *list[5];
     list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
     list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.3)));
-    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
-    list[3] = new sphere(vec3(-1,0,-1), 0.5, new lambertian(vec3(0.8, 0.8, 0.8)));
+    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.6, 1), 0.3));
+    list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
+    list[4] = new sphere(vec3(-1,0,-1), -0.49, new dielectric(1.5));
     
-    hitable *world = new hitable_list(list, 4);
+    hitable *world = new hitable_list(list, 5);
     camera cam;
     
     for (int j = ny - 1; j >= 0; j--) {
