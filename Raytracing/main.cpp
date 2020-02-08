4e830f0b36b130ea2f82c4bd2a917ad20663f380
Alexander Shevchenko <alex.shevchenko@ubico.io>
2020-02-08 01:46:50 -0500

    Removed unused commented out method

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -170,19 +170,6 @@ public:
     vec3 B;
 };
 
-//bool hit_sphere(const vec3& center, float radius, const ray& r){
-//    vec3 oc = r.origin() - center;
-//    float a = dot(r.direction(), r.direction());
-//    float b = 2.0 * dot(oc, r.direction());
-//    float c = dot(oc, oc) - radius*radius;
-//    float discriminant = b*b - 4*a*c;
-//    if (discriminant < 0) {
-//        return -1.0;
-//    }else{
-//        return (-b - sqrt(discriminant)) / (2.0 * a);
-//    }
-//}
-
 class material;
 
 struct hit_record{
